ace0e55f686b290ab91c8237372a9d55b973cd28
Esta Rosa Msimango <esta@dsa.tshimologong.joburg>
2022-03-08 14:44:44 +0200

    second commit :fire:

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let screen = document.querySelector('#output-box');
-    //add tasks
-    const addTask = document.forms['add-task'];
-    addTask.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const task = addTask.querySelector('input[type="text"').value;
+let screen = document.querySelector('#output-box');
+const addTask = document.forms['add-task'];//add tasks
+const uList = document.createElement('ul')//create unordered list
+const clearTask = document.querySelector('add');
 
+addTask.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const task = addTask.querySelector('input[type="text"').value;
+
+    if(task != "") {
         //create element
-        const uList = document.createElement('ul')
         const listTask = document.createElement('li')
         const taskName = document.createElement('span')
-        const deleteBtn = document.createElement('button')
+        var deleteBtn = document.createElement('button')
 
         //add content
         taskName.textContent = task;
@@ -24,38 +25,29 @@ document.addEventListener('DOMContentLoaded', function(){
         listTask.appendChild(deleteBtn);
         uList.appendChild(listTask);
         screen.appendChild(uList);
-    })
-    //event bubbling
-    let taskList = document.querySelector('#output-box ul');
-    li.addEventListener('click', function(e) {
-        if(e.target.className == 'delete') {
-            const listTask = e.target.parentElement;
-            taskList.removeChild(listTask);
-        }    
-    })
-    
-    //delete button
-    let btns = document.querySelectorAll('#output-box .delete');
-    Array.from(btns).forEach(function(btn) {
-        btn.addEventListener('click', function(e){
-            const list = e.target.parentElement;
-            list.parentNode.removeChild(list);
-        })
-    })
- 
-    //search an item
-    let searchTask = document.forms['search-task'];
-    searchTask.addEventListener('keyup', function(e) {
-        const term = e.target.value.toLowerCase();
-        const tasks = taskList.getElementsByTagName('li');
-        Array.from(tasks).forEach(function(task) {
-            const taskTitle = tasks.firstChildElement.textContent;
-            if(taskTitle.toLowerCase().indexOf(term) != -1) {
-                book.style.display = 'block';
-            }else {
-                book.style.display = 'none';
-            }
-        })
-    })
+    }   
+})
 
+//event bubbling //delete button
+const taskList = document.querySelector('#output-box ul');
+taskList.addEventListener('click', function(e) {
+    if(e.target.className == 'delete') {
+        const listTask = e.target.parentElement;
+        taskList.removeChild(listTask);
+    }    
+})
+
+//search an item
+const searchTask = document.forms['search-task'].querySelector('input');
+searchTask.addEventListener('keyup', function(e) {
+    const term = e.target.value.toLowerCase();
+    const tasks = taskList.getElementsByTagName('li');
+    Array.from(tasks).forEach(function(task) {
+        const taskTitle = task.firstChildElement.textContent;
+        if(taskTitle.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        }else {
+            book.style.display = 'none';
+        }
+    })
 })
